fb17b37565f4904a13735e2bfee88ee0a0298f6b
Emilie22 <daguerre.emilie@gmail.com>
2019-01-11 18:21:46 +0100

    jour1

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Question;
-use App\Form\QuizType;
+use App\Entity\Answer;
+use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\User;
 
 class QuizController extends AbstractController
 {
@@ -19,6 +21,29 @@ class QuizController extends AbstractController
     	$repository = $this->getDoctrine()->getRepository(Question::class);
     	$questions = $repository->findAll();
 
+    	$repositoryResponse = $this->getDoctrine()->getRepository(Answer::class);
+
+		$get = $request->query->all();
+		$answerObj=[];
+		foreach ($get as $key => $value){
+			$answerObj[] = $repositoryResponse->find($value);
+		}
+
+		$userAnswer = new User();
+		foreach ($answerObj as $key => $value){
+			$userAnswer->addAnswer($value);
+		}
+
+		$entityManager = $this->getDoctrine()->getManager();
+		$entityManager->persist($userAnswer);
+		$entityManager->flush();
+
+
+
+
         return $this->render('quiz/index.html.twig', ['questions' => $questions]);
     }
+
+
+
 }
