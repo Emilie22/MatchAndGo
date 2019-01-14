a9bfe4f68729975345fb5c05f91409fe3966ce3a
Soraya <sorayabenaziza@gmail.com>
2019-01-14 18:29:47 +0100

    création profil + admin concept

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Validator\Constraints\IsTrue;
 
 class ProfileType extends AbstractType
@@ -19,19 +20,24 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname', TextType::class, array('label' => 'Votre prénom'))
-            ->add('lastname', TextType::class, array('label' => 'Votre nom'))
-            ->add('city', TextType::class, array('label' => 'Votre ville'))
-            ->add('birthday', DateType::class, array('label' => 'Votre date de naissance'))
-            ->add('gender', CheckboxType::class, array(
-                'mapped' => false,
-                'constraints' => new IsTrue(),
-                'label' => 'Homme'))
-            ->add('phone', TextType::class, array('label' => 'Votre numéro de téléphone'))
-            ->add('picture', FileType::class, array('data_class'=> null, 'label' => 'Votre photo'))
-            ->add('description', TextareaType::class, array('label' => 'Parlez-nous un peu de vous !'))
-            ->add('countries', TextType::class, array('label' => 'Pays visités'))
-            ->add('save', SubmitType::class, array('label' => 'Enregistrer'))
+            ->add('firstname', TextType::class, array('label' => 'Prénom'))
+            ->add('lastname', TextType::class, array('label' => 'Nom'))
+            ->add('city', TextType::class, array('label' => 'Ville'))
+            ->add('birthday', DateType::class, array(
+                'label' => 'Votre date de naissance',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd'))
+            ->add('gender', ChoiceType::class, array(
+                'choices' =>array(
+                    'femme' => 'femme',
+                    'homme' => 'homme')))
+            ->add('phone', TextType::class, array('label' => 'Numéro de téléphone'))
+            ->add('picture', FileType::class, array(
+                'data_class'=> null, 
+                'label' => 'Choisis ta photo de profil', 'required' => false))
+            ->add('description', TextareaType::class, array('label' => 'Parle-nous un peu de toi !'))
+            ->add('countries', TextType::class, array('label' => 'Quels pays as-tu visités ?'))
+            ->add('save', SubmitType::class, array('label' => 'Créer mon profil'))
         ;
     }
 
